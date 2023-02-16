a79c8547d0059242be1524ccacbea3c258c6b565
andwis93 <andwis17@gmail.com>
2023-02-16 12:48:19 +0100

    Hexadecimal test fix

--- src/test/java/HexadecimalConverterTest.java
+++ src/test/java/HexadecimalConverterTest.java
@@ -1,11 +1,19 @@
 import converter.HexadecimalConverter;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HexadecimalConverterTest {
 
     @Test
     void testHexadecimalConverter(){
+        //Given
         HexadecimalConverter hexadecimalConverter = new HexadecimalConverter();
-        System.out.println(hexadecimalConverter.convertToHexadecimal(1548689874648631287L));
+
+        //When
+       String hexadecimal = hexadecimalConverter.convertToHexadecimal(1548689874648631287L);
+
+       //Then
+        assertEquals("157E0D3C55AD07F7", hexadecimal);
     }
 }
