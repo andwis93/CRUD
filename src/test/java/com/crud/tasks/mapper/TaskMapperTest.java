1409997d6cfc9f5096d27fe05cdc0ae5fcbefa8d
andwis93 <andwis17@gmail.com>
2023-02-23 12:32:12 +0100

    6.0

--- src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -14,42 +14,42 @@ public class TaskMapperTest {
     @Autowired
     private TaskMapper taskMapper;
 
-//    @Test
-//    void testMapToTaskDtoList() {
-//        //Given
-//        List<Task> taskList = List.of(new Task(1L, "Task1", "Description1"),
-//                new Task(2L, "Task2", "Description2"));
-//
-//        //When
-//        List<TaskDto> taskDtos = taskMapper.mapToTaskDtoList(taskList);
-//
-//        //Then
-//        assertEquals(2, taskDtos.size());
-//    }
+    @Test
+    void testMapToTaskDtoList() {
+        //Given
+        List<Task> taskList = List.of(new Task(1L, "Task1", "Description1"),
+                new Task(2L, "Task2", "Description2"));
 
-//    @Test
-//    void testMapToTaskDto() {
-//        //Given
-//        Task task = new Task(1L, "Task", "Description");
-//
-//        //When
-//        TaskDto taskDto = taskMapper.mapToTaskDto(task);
-//
-//        //Then
-//        assertNotNull(taskDto);
-//        assertEquals(1L, taskDto.getId());
-//    }
+        //When
+        List<TaskDto> taskDtos = taskMapper.mapToTaskDtoList(taskList);
 
-//    @Test
-//    void testMapToTask() {
-//        //Given
-//        TaskDto taskDto = new TaskDto(1L, "Task", "Content");
-//
-//        //When
-//        Task task = taskMapper.mapToTask(taskDto);
-//
-//        //Then
-//        assertNotNull(task);
-//        assertEquals(1L, task.getId());
-//    }
+        //Then
+        assertEquals(2, taskDtos.size());
+    }
+
+    @Test
+    void testMapToTaskDto() {
+        //Given
+        Task task = new Task(1L, "Task", "Description");
+
+        //When
+        TaskDto taskDto = taskMapper.mapToTaskDto(task);
+
+        //Then
+        assertNotNull(taskDto);
+        assertEquals(1L, taskDto.getId());
+    }
+
+    @Test
+    void testMapToTask() {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "Task", "Content");
+
+        //When
+        Task task = taskMapper.mapToTask(taskDto);
+
+        //Then
+        assertNotNull(task);
+        assertEquals(1L, task.getId());
+    }
 }
